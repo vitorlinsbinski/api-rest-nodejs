b264e071b5cadd9db6a0045d00f5ce341b7bac93
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-01-30 14:53:44 -0400

    add: routes and cookie middleware

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,28 +2,44 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { knex } from '../database';
 import { randomUUID } from 'node:crypto';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
+
+// Cookies -> Formas de manter contexto entre requisições
+// Muitos websites armazenam um id durante a navegação
+// Com esse id, é possível validar que uma mesma pessoa fez certas requisições
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select();
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const { sessionId } = req.cookies;
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select();
 
     return { transactions };
   });
 
-  app.get('/:id', async (req) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (req) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = getTransactionParamsSchema.parse(req.params);
 
-    const transaction = await knex('transactions').where('id', id).first();
+    const { sessionId } = req.cookies;
+
+    const transaction = await knex('transactions')
+      .where({ session_id: sessionId, id })
+      .first();
 
     return { transaction };
   });
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
+
     const summary = await knex('transactions')
+      .where('session_id', sessionId)
       .sum('amount', { as: 'amount' })
       .first();
 
@@ -39,10 +55,22 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(req.body);
 
+    let sessionId = req.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     });
 
     // HTTP codes
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
+
 import { env } from './env';
 import { transactionRoutes } from './routes/transactions';
 
 const app = fastify();
+
+app.register(cookie);
+
+app.addHook('preHandler', async (req) => {
+  console.log(`[${req.method}] ${req.url}`);
+});
 
 app.register(transactionRoutes, {
   prefix: '/transactions',
